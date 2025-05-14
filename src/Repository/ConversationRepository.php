08de6905d948cf811e24a3ff9116729afac7128e
fabien-design <fabienrozier60@gmail.com>
2025-05-14 16:26:17 +0200

    refactor: clean up ConversationRepository by removing commented-out code and improving method structure

--- src/Repository/ConversationRepository.php
+++ src/Repository/ConversationRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Conversation;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -16,28 +17,33 @@ class ConversationRepository extends ServiceEntityRepository
         parent::__construct($registry, Conversation::class);
     }
 
-    //    /**
-    //     * @return Conversation[] Returns an array of Conversation objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('c.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    /**
+     * Trouve toutes les conversations d'un utilisateur
+     */
+    public function findByUser(User $user): array
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.seller = :user OR c.buyer = :user')
+            ->setParameter('user', $user)
+            ->getQuery()
+            ->getResult();
+    }
 
-    //    public function findOneBySomeField($value): ?Conversation
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    /**
+     * Trouve une conversation entre un acheteur et un vendeur à propos d'une offre
+     */
+    public function findByOfferAndUsers(int $offerId, int $buyerId, int $sellerId): ?Conversation
+    {
+        $qb = $this->createQueryBuilder('c');
+        
+        return $qb
+            ->andWhere('c.offer = :offerId')
+            ->andWhere('c.buyer = :buyerId')
+            ->andWhere('c.seller = :sellerId')
+            ->setParameter('offerId', $offerId)
+            ->setParameter('buyerId', $buyerId)
+            ->setParameter('sellerId', $sellerId)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 }
